efac12971dfe10d710578ceec9ee152797db9761
rahulshukla <rahul.shukla90@outlook.com>
2022-02-27 10:53:41 -0800

    Checking Code for Multi table View

---
+++ StockDetails/StockDetails/Constants/APIkeys.swift
@@ -0,0 +1,11 @@
+//
+//  APIkeys.swift
+//  StockDetails
+//
+//  Created by Rahul Shukla on 2/26/22.
+//
+
+import Foundation
+
+
+let apiKeys = "8e59fe7bea31dad218b69ad09f13c5fe"
---
+++ StockDetails/StockDetails/Constants/URLs.swift
@@ -0,0 +1,11 @@
+//
+//  URLs.swift
+//  StockDetails
+//
+//  Created by Rahul Shukla on 2/26/22.
+//
+
+import Foundation
+
+
+let profile = 
